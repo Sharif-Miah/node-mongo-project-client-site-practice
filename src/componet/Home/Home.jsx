41eaf3a71020a6cb76c7e6b1c0cecdc4516231a6
farhan sharif <farhansharif1410@gmail.com>
2022-11-06 20:19:09 +0530

    project setup

---
+++ src/componet/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                node-mongo-project-server-site-practice
+            </div>
+        </div>
+    );
+};
+
+export default Home;
